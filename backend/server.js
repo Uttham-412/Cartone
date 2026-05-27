00071ae56104d69cc8b9b7f457b57eedf1c7ac0e
UTTHAM-12 <uttham1206@gmail.com>
2026-05-27 20:33:00 +0530

    feat: setup express backend server

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+    res.json({
+        message: "OrbitCart API is running",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
