6f95b51fc644845bd3f8b57a051498a8fdfe983b
UTTHAM-12 <uttham1206@gmail.com>
2026-05-27 23:43:54 +0530

    refactor: organize pages into separate components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function Home() {
-  return <h1>CART One</h1>;
-}
-
-function Login() {
-  return <h1>Login Page</h1>;
-}
-
-function Register() {
-  return <h1>Register Page</h1>;
-}
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>CART One</h1>
+            <p>Welcome to CART One Shopping App</p>
+        </div>
+    );
+}
+
+export default Home;
